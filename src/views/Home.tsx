d6242e365e98b0aca5e748f8e9e9fca05571ac80
Michał Dziechciarz <michal.dziechciarz1@gmail.com>
2021-10-18 17:15:44 +0200

    Rename AdsBanner to AdBanner

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from 'components/Navbar/Navbar';
-import AdBanner from 'components/AdsBanner/AdBanner';
+import AdBanner from 'components/AdBanner/AdBanner';
 
 const Home = (): JSX.Element => (
   <>
